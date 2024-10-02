d1177c7e0e9310694da90904c89654efaad7c191
TetewHeroez <teosofihidayahagung@gmail.com>
2024-10-02 23:24:51 +0700

    syulit

---
+++ Asisten Laboratorium/Alpro 1/Solution/Week 2 Solution/No1.java
@@ -0,0 +1,9 @@
+public class No1 {
+    public static void main(String[] args) {
+        int h=2, k=3, r=5;
+        int A = -2*h;
+        int B = -2*k;
+        int C = h*h + k*k - r*r;
+        System.out.println("Persamaan lingkaran: " + A + "x + " + B + "y + " + C + " = 0");
+    }
+}
